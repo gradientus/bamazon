64011eac22165c3b8bfc871f9efb1c6edf0f3572
Phil Mauracher <gradientus@gmail.com>
2019-09-08 14:27:14 -0600

    doThis(), allProducts(), anythingElse(), buhBye() are all fully functional in the bamazonManager.js file.

--- bamazonManager.js
+++ bamazonManager.js
@@ -11,16 +11,119 @@ var connection = mysql.createConnection({
   database: "bamazon_db"
 });
 
-doWhat();
+//connect to database, start process of managing inventory
+connection.connect(err => {
+  if (err) {
+    throw err;
+  }
+  console.log(`\nConnected as ID ${connection.threadId}\n`);
+  doThis();
+});
 
-function doWhat() {}
+//ask what the user wants to do
+function doThis() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "choose",
+        message:
+          "Welcome to Bamazon Inventory Manager!  Please choose an action to take.",
+        choices: [
+          "List All Inventory",
+          "List Low Inventory",
+          "Add Inventory to Existing Products",
+          "Add a New Product",
+          "Exit the Bamazon Inventory Manager"
+        ],
+        default: 0
+      }
+    ])
+    .then(answer => {
+      let doThis = answer.choose;
+      switch (doThis) {
+        case "List All Inventory":
+          allProducts();
+          break;
 
-function allProducts() {}
+        case "List Low Inventory":
+          lowInventory();
+          break;
 
-function lowInventory() {}
+        case "Add Inventory to Existing Products":
+          addInventory();
+          break;
 
-function addInventory() {}
+        case "Add a New Product":
+          newProduct();
+          break;
 
-function newProduct() {}
+        case "Exit the Bamazon Inventory Manager":
+          buhBye();
+          break;
 
-function anythingElse() {}
+        default:
+          console.log("\nPlease make a choice.");
+      }
+    });
+}
+
+//list all inventory
+function allProducts() {
+  connection.query("SELECT * FROM products", (err, response) => {
+    if (err) {
+      throw err;
+    }
+    for (var i = 0; i < response.length; i++) {
+      //TODO: look into map/reduce/etc, instead of loop
+      console.log(
+        `\nID: ${response[i].item_id}  `,
+        `Product: ${response[i].product_name}  `,
+        `$${response[i].price}`
+      );
+    }
+    anythingElse();
+  });
+}
+
+//list low inventory
+function lowInventory() {
+  console.log("\nlowInventory");
+}
+
+//add inventory to existing products
+function addInventory() {
+  console.log("\naddInventory");
+}
+
+//add new products to inventory
+function newProduct() {
+  console.log("\nnewProduct");
+}
+
+//does the user want to continue using the application or not
+function anythingElse() {
+  inquirer
+    .prompt([
+      {
+        type: "confirm",
+        name: "anythingElse",
+        message: "Would you like to do anything else?",
+        default: true
+      }
+    ])
+    .then(more => {
+      let answer = more.anythingElse;
+      if (answer) {
+        doThis();
+      } else {
+        buhBye();
+      }
+    });
+}
+
+//break the connection, everything is done
+function buhBye() {
+  console.log("\n\nThanks for using the Bamazon Inventory Manager.  Goodbye.");
+  connection.end();
+}
